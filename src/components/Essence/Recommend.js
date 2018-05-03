c341f9676305f24783533735c2b4b3fa236d2737
Charles <>
2018-05-03 21:11:36 +0800

    首页布局

--- src/components/Essence/Recommend.js
+++ src/components/Essence/Recommend.js
@@ -13,7 +13,6 @@ import { connect } from 'react-redux';
 import { width } from '../../configs/Device';
 import { loadRecommendList } from '../../actions/Essence/Recommend'
 
-
 class Recommend extends Component {
     componentDidMount() {
         const {data, loadRecommendList } = this.props;
@@ -48,68 +47,81 @@ class Recommend extends Component {
         return item.text + index;
     }
 
-    _renderItem({item}) {
-        if (item.type === 'text') {
+    _renderItemContent(item) {
+        if (item.type === 'gif') {
             return (
-                <TouchableOpacity 
-                    activeOpacity={0.7}
-                    style={styles.item}
-                    onPress={() => this.goToDetail(item)}
-                >
-                    <View style={styles.itemText}>
-                        <Text>{item.text}</Text>
-                    </View>
-                </TouchableOpacity>
+                <View>
+                    <Text style={styles.itemText}>{item.text}</Text>
+                    <Image 
+                        source={{uri: item.gif.images[0]}}
+                        style={{width: width-20, height: (width-20) / item.gif.width * item.gif.height}} 
+                    />
+                </View>
             )
-        } else if (item.type === 'gif') {
+        } else if (item.type === 'image') {
             return (
-                <TouchableOpacity 
-                    activeOpacity={0.7}
-                    style={styles.item}
-                    onPress={() => this.goToDetail(item)}
-                >
-                    <View style={styles.itemImage}>
-                        <Image 
-                            source={{uri: item.gif.images[0]}}
-                            style={{width: width-20, height: width / item.gif.width * item.gif.height}} 
-                        />
-                    </View>
-                </TouchableOpacity>
+                <View>
+                    <Text style={styles.itemText}>{item.text}</Text>
+                    <Image 
+                        source={{uri: item.image.big[0]}}
+                        style={{width: width-20, height: (width-20) / item.image.width * item.image.height}} 
+                    />
+                </View>
             )
         } else if (item.type === 'video') {
             return (
-                <TouchableOpacity 
-                    activeOpacity={0.7}
-                    style={styles.item}
-                    onPress={() => this.goToDetail(item)}
-                >
-                    <View style={styles.itemVideo}>
-                        <Text style={{marginBottom: 10}}>{item.text}</Text>
-                        <Image 
-                            source={{uri: item.video.thumbnail[0]}}
-                            style={{width: width-20, height: width / item.video.width * item.video.height}} 
-                        />
-                    </View>
-                </TouchableOpacity>
+                <View>
+                    <Text style={styles.itemText}>{item.text}</Text>
+                    <Image 
+                        source={{uri: item.video.thumbnail[0]}}
+                        style={{width: width-20, height: (width-20) / item.video.width * item.video.height}} 
+                    />
+                </View>
             )
         } else {
             return (
-                <TouchableOpacity 
-                    activeOpacity={0.7}
-                    style={styles.item}
-                    onPress={() => this.goToDetail(item)}
-                >
-                    <View style={styles.itemText}>
-                        <Text>{item.text}</Text>
-                    </View>
-                </TouchableOpacity>
+                <View>
+                    <Text style={{fontSize: 12}}>{item.text}</Text>
+                </View>
             )
         }
-		
     }
 
-    goToDetail(item) {
-        this.props.navigation.navigate('Detail', {title: item.theme_name});
+    _renderItem({item}) {
+        return (
+            <View style={styles.item}>
+                <TouchableOpacity 
+                    activeOpacity={0.7}
+                    onPress={() => this._goToUser(item.u)}
+                >
+                    <View style={{height: 50, marginBottom: 10, flexDirection: 'row', alignItems: 'center'}}>
+                        <Image 
+                            source={{uri:item.u.header[0]}}
+                            style={{width: 30, height: 30, borderRadius: 15}}
+                        />
+                        <View style={{marginLeft: 10, justifyContent: 'space-evenly', height: 50}}>
+                            <Text style={{fontSize: 12, color: '#000'}}>{item.u.name}</Text>
+                            <Text style={{fontSize: 10, color: '#333'}}>{item.passtime}</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    activeOpacity={0.7}
+                    onPress={() => this._goToDetail(item)}
+                >
+                    {this._renderItemContent(item)}
+                </TouchableOpacity>
+            </View>
+            
+        )
+    }
+
+    _goToUser(user) {
+        this.props.navigation.navigate('Detail', {title: user.name});
+    }
+
+    _goToDetail(item) {
+        this.props.navigation.navigate('Detail', {title: item.text});
     }
 };
 
@@ -118,18 +130,12 @@ const styles = StyleSheet.create({
         flex: 1, 
     },
     item: {
-        // alignItems: 'center',
-        // backgroundColor: '#fff',
         padding: 10,
+        backgroundColor: '#fff',
     },
     itemText: {
-
-    },
-    itemImage: {
-
-    },
-    itemVideo: {
-
+        marginBottom: 10,
+        fontSize: 12,
     },
 })
 
