bb0165d03d87f304d452d39cd94109b2922c7458
Charles <>
2018-05-05 17:06:49 +0800

    一级页面基本完成

--- src/configs/API.js
+++ src/configs/API.js
@@ -1,6 +1,7 @@
 const apiPath = 'http://d.api.budejie.com/topic/';
 
 const API = {
+    //精选
     essence: {
         recommend: apiPath + 'list/jingxuan/1/bs0315-iphone-4.5.9/',
         picture: apiPath + 'list/jingxuan/10/bs0315-iphone-4.5.9/',
@@ -8,6 +9,7 @@ const API = {
         joke: apiPath + 'tag-topic/63674/hot/bs0315-iphone-4.5.9/',
         hot: apiPath + 'list/remen/1/bs0315-iphone-4.5.9/',
     },
+    //最新
     new: {
         all: apiPath + 'list/zuixin/1/bs0315-iphone-4.5.9/',
         video: apiPath + 'list/zuixin/41/bs0315-iphone-4.5.9/',
@@ -15,6 +17,7 @@ const API = {
         joke: apiPath + 'list/zuixin/29/bs0315-iphone-4.5.9/',
         movie: apiPath + 'tag-topic/407/new/bs0315-iphone-4.5.9/',
     },
+    //穿越
     through: {
         all: apiPath + 'list/chuanyue/1/bs0315-iphone-4.5.9/',
         video: apiPath + 'list/chuanyue/41/bs0315-iphone-4.5.9/',
