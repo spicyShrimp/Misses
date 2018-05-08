23aeec9c40c7d45eb239b51e5fdb96c61b5c9630
spicyboiledfish <327434855@qq.com>
2018-05-09 00:01:08 +0800

    导航栏改成透明

--- src/components/Friend/FriendDetail.js
+++ src/components/Friend/FriendDetail.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import NavItem from '../Navigator/NavItem';
+import { ios, statusBarHeight } from '../../configs/Device';
 
 export default class FriendDetail extends Component {
     static navigationOptions = ({navigation}) => {
 		const { params } = navigation.state;
 		return {
             headerTitle: params.title,
-            headerRight: <NavItem />,
+            headerStyle:{ 
+                borderBottomWidth: 0,
+                elevation: 0,
+                paddingTop: ios ? 0 : statusBarHeight,
+                height: 44 + (ios ? 0 : statusBarHeight),
+                backgroundColor: 'transparent'
+            }
 		}
 	};
 
